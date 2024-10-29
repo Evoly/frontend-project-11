a50bca6849c64963b68dfbc21656a5331c127097
evoly <svetoch.sv@gmail.com>
2024-10-29 08:39:51 +0700

    add devDependencies

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+# RSS Aggregator
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Evoly/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Evoly/frontend-project-11/actions)
+
+### Codeclimate:
+[![Maintainability](https://api.codeclimate.com/v1/badges/fabe709fb29810642db6/maintainability)](https://codeclimate.com/github/Evoly/frontend-project-11/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    main: ['./src/js/index.js', './src/style.scss'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[hash][ext]'
+        }
+      },
+
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist/'),
+    },
+    compress: true,
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+    watchFiles: {
+      paths: ['src/**/*.*'],
+    },
+    open: {
+      app: {
+        name: 'google-chrome',
+      },
+    },
+  },
+};
