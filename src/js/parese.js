5487320dc7e26049aa017d95cf6555d13bd58d6a
evoly <16191619+Evoly@users.noreply.github.com>
2024-11-03 12:03:06 +0700

    add parse

---
+++ src/js/parese.js
@@ -0,0 +1,25 @@
+const parse = (data) => {
+  const parser = new DOMParser();
+  const content = parser.parseFromString(data.contents, 'text/xml');
+  const error = content.querySelector('parsererror');
+  if (error) {
+    const parseError = new Error();
+    parseError.name = 'parseError'
+    throw parseError
+  }  
+  const items = content.querySelectorAll('item');
+  const itmesList = [...items].map((item) => {
+    return {
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent
+    }
+  });
+  return {
+    title: content.querySelector('title').textContent,
+    description: content.querySelector('description').textContent,
+    items: itmesList
+  }
+};
+
+export default parse;
